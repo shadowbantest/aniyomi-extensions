3c293614618309f733902fc5fc804ba9d5046a89
shadowbantest <mohamedsharif2121@gmail.com>
2025-06-11 00:33:48 +0100

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,25 +8,8 @@ File(rootDir, "lib").eachDir { include("lib:${it.name}") }
 // Load all modules under /lib-multisrc
 File(rootDir, "lib-multisrc").eachDir { include("lib-multisrc:${it.name}") }
 
-if (System.getenv("CI") != "true") {
-    // Local development (full project build)
-
-    /**
-     * Add or remove modules to load as needed for local development here.
-     */
-    loadAllIndividualExtensions()
-    // loadIndividualExtension("all", "jellyfin")
-} else {
-    // Running in CI (GitHub Actions)
-
-    val chunkSize = System.getenv("CI_CHUNK_SIZE").toInt()
-    val chunk = System.getenv("CI_CHUNK_NUM").toInt()
-
-    // Loads individual extensions
-    File(rootDir, "src").getChunk(chunk, chunkSize)?.forEach {
-        loadIndividualExtension(it.parentFile.name, it.name)
-    }
-}
+// Force loading all extensions (even on GitHub)
+loadAllIndividualExtensions()
 
 fun loadAllIndividualExtensions() {
     File(rootDir, "src").eachDir { dir ->
